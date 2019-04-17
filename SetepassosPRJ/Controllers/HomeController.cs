cf7ca9f8f1e4f1a2d4c28e79a2dea4ec7e396982
Maria Serrão <160323018@estudantes.ips.pt>
2019-04-17 17:20:13 +0100

    minis e regras

--- SetepassosPRJ/Controllers/HomeController.cs
+++ SetepassosPRJ/Controllers/HomeController.cs
@@ -39,5 +39,10 @@ namespace SetepassosPRJ.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Regras()
+        {
+           return View();
+        }
     }
 }
