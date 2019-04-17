45b734f9e9de8378676f2d22c27a67f2d3276da8
Mafalda Sousa <180323044@estudantes.ips.pt>
2019-04-17 20:07:08 +0100

    Tentei sobrepor as imagens...but failed to...

--- SetepassosPRJ/Controllers/JogoController.cs
+++ SetepassosPRJ/Controllers/JogoController.cs
@@ -28,43 +28,6 @@ namespace SetepassosPRJ.Controllers
                 return View();
         }
 
-         public IActionResult JogoAlternativoInimigo()
-         {
-             if (ModelState.IsValid)
-             {
-                List<Jogo> jogos = Repositorio.Jogo;
-                Jogo jogoAlternativo = jogos[jogos.Count - 1]; //Devolve o último elemento guardado na lista
-                return View("JogoAlternativoInimigo", jogoAlternativo);
-             }
-             else
-                 return View("Jogo");
-         }
-
-
-        public IActionResult JogoAlternativoPocao()
-        {
-            if (ModelState.IsValid)
-            {
-                List<Jogo> jogos = Repositorio.Jogo;
-                Jogo jogoAlternativo = jogos[jogos.Count - 1]; //Devolve o último elemento guardado na lista
-                return View("JogoAlternativoPocao", jogoAlternativo);
-            }
-            else
-                return View("Jogo");
-        }
-
-        public IActionResult JogoAlternativoChave()
-        {
-            if (ModelState.IsValid)
-            {
-                List<Jogo> jogos = Repositorio.Jogo;
-                Jogo jogoAlternativo = jogos[jogos.Count - 1]; //Devolve o último elemento guardado na lista
-                return View("JogoAlternativoChave", jogoAlternativo);
-            }
-            else
-                return View("Jogo");
-        }
-
         public IActionResult HighScore()
         {
             return View();
