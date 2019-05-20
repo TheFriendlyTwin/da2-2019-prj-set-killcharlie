f9e3939954eda1fab7db8cb1b4a1a5a3ab8bbc24
Maria Serrão <160323018@estudantes.ips.pt>
2019-05-20 14:40:24 +0100

    newgamerequest

---
+++ SetepassosPRJ/Models/NewGameRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SetepassosPRJ.Models
+{
+    public class NewGameRequest
+    {
+        public string PlayerName { get; set; }
+        public string PlayerClass { get; set; }
+        public string Teamkey { get; set; }
+
+    
+        public NewGameRequest(string playerName, string playerClass, string teamkey)
+        {
+        PlayerName = playerName;
+        PlayerClass = playerClass;
+        Teamkey = teamkey;
+         }
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
