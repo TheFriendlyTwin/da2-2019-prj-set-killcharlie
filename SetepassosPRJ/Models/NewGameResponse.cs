ea0a95d2ddbbafefd3ba04bcdce3e00ac7525b08
Mafalda Sousa <180323044@estudantes.ips.pt>
2019-05-20 14:44:45 +0100

    Criar a classe NewGameResponse

---
+++ SetepassosPRJ/Models/NewGameResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SetepassosPRJ.Models
+{
+    public class NewGameResponse
+    {
+        public int GameID { get; set; }
+        public int RoundNumber { get; set; }
+        public int Action { get; set; }
+        public int Result { get; set; }
+        public bool FoundNumber { get; set; }
+        public bool FoundItem { get; set; }
+        public bool FoundKey { get; set; }
+        public bool FoundPotion { get; set; }
+        public int GoldFound { get; set; }
+        public int EnemyDamageSuffered { get; set; }
+        public int EnemyHealthPoints { get; set; }
+        public int EnemyAttackPoints { get; set; }
+        public int EnemyLuckPoints { get; set; }
+        public int ItemHealthEffect { get; set; }
+        public int ItemAttackEffect { get; set; }
+        public int ItemLuckEffect { get; set; }
+        
+    }
+}
