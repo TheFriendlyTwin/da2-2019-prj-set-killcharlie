596447ab86ff9ec2afbb1e1c03696cecda5cd0be
Marta Sousa <170323021@estudantes.ips.pt>
2019-05-20 14:42:34 +0100

    MyGameHTTPClient

---
+++ SetepassosPRJ/Models/MyGameHTTPClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace SetepassosPRJ.Models
+{
+    public class MyGameHTTPClient
+    {
+        private static string baseAdress = "http://193.137.46.2/swagger";
+
+        private static HttpClient client;
+
+        public static HttpClient Client
+        {
+            get
+            {
+                if (client==null)
+                {
+                    client = new HttpClient();
+                    client.BaseAddress = new Uri(baseAdress);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                }
+                return client;
+            }
+        }
+    }
+}
