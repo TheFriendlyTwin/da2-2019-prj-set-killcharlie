53fc863526c4cd874ef87681ef06f2c8ff1d599c
Marta Sousa <170323021@estudantes.ips.pt>
2019-05-22 12:52:37 +0100

    Fiz a classe PlayApiRequest

---
+++ SetepassosPRJ/Models/PlayApiRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SetepassosPRJ.Models
+{
+    public class PlayApiRequest
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public int Playerastion { get; set; }
+
+        public PlayApiRequest(int id, string key,int playeraction)
+            {
+                Id = id;
+                Key = "757153521a8f474da578fd7ce77dfadc";
+                Playerastion = playeraction;
+            }
+    }
+}
