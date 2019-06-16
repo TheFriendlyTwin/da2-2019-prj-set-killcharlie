657d80e07ebc2ee69083a0ebe06fa573bf2897cd
Mafalda Sousa <180323044@estudantes.ips.pt>
2019-06-16 02:33:27 +0100

    Criei um controller para o Modo Autónomo, assim como uma pasta nas views para o controller e duas views: uma para Iniciar o modo autónomo e outra para os resultados do jogo/resultados globais

---
+++ SetepassosPRJ/Controllers/ModoAutonomoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SetepassosPRJ.Models;
+
+namespace SetepassosPRJ.Controllers
+{
+    public class ModoAutonomoController : Controller
+    {
+        [HttpGet]
+        public IActionResult NovoAutoJogo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NovoAutoJogo(int rondas)
+        {
+            return View("Resultados");
+        }
+    }
+}
---
+++ SetepassosPRJ/Models/ModoAutonomo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SetepassosPRJ.Models
+{
+    public class ModoAutonomo : Jogo
+    {
+        public int NumeroRondas { get; set; }
+    }
+}
